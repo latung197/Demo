a06c7197c76f8a58c215ccbfb7609a5734c0393d
DESKTOP-QV3DI8R\Coasia_MI_Laptop <levantung0000@gmail.com>
2023-09-15 16:39:17 +0700

    z

--- js/myJs.js
+++ js/myJs.js
@@ -1,17 +1,17 @@
 const textConfig = {
-  text1: "He luu cậu Lại là Tươi đây!",
-  text2: "Tớ có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa.",
-  text3: "Cậu yêu tớ có phải không nào ._.",
-  text4: "Nếu cậu ko trả lời mà thoát ra tức là muốn làm vợ tớ rùi đó nha :v",
-  text5: "Cậu mơ à???",
-  text6: "Yêu ơi là yêu <3",
-  text7: "lí do cậu thích tớ đi :vvvv",
-  text8: "Gửi cho tớ <3",
-  text9: "Vì cậu đẹp try vlllll",
-  text10: "Tớ biết mà ^^ Yêu cậu 300.000",
+  text1: "안녕 오빠~~ 저는 오빠 공주예용!",
+  text2: "제가 오빠한테 물어본게 있어요. 진심으로 대답해 야돼요~~~ .",
+  text3: "오빠가 저를 많이 많이 사랑해요? ._.",
+  text4: "진심으로 대답하지 않으면은 때릴게예요 진짜로 :v",
+  text5: "너 꿈 꾸고 있냐???",
+  text6: "당연히 사랑하지용. 내 공주님인데... <3",
+  text7: "저를 사랑하는 이유는 뭐예요?  :vvvv",
+  text8: "알려 주세요~~ <3",
+  text9: "왜냐하면 넌 세상에 제일 귀여운 여자양",
+  text10: "알고 있거드용 ^^ 사랑해요 오빠 300.000",
   text11:
-    "Tối nay tớ qua đón cậu đi chơi nhaa :v Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàooo",
-  text12: "Okii lunn <3",
+    "우리 오늘 저녁에 놀러 가자~ :v 고 고고 또 말하고 싶은 말이 있이요?",
+  text12: "콜 <3",
 };
 
 $(document).ready(function () {
